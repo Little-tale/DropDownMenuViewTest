fbd0393b5000cdfe06ba56ea7a29631481c0a8f7
Kim JaeHyung <you3192001@icloud.com>
2024-08-27 17:15:48 +0900

    Initial Commit

---
+++ DropDownMenuViewTest/DropDownMenuViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownMenuViewTestApp.swift
+//  DropDownMenuViewTest
+//
+//  Created by Jae hyung Kim on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DropDownMenuViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
